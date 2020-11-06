3538ad2cf5da9966f20ff7bef397d7ca050d569a
dmotornyi <motorny2015@gmail.com>
2020-11-06 11:43:01 +0200

    Change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template("index.html")
 
@@ -11,6 +11,10 @@ def index():
 def about():
     return render_template("hardware.html")
 
+
+@app.route('/')
+def login():
+    return render_template("Login.html")
 
 
 if __name__ == "__main__":
