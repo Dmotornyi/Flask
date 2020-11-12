c65d53dddeaf1a9d2043fe07de08533972b4c869
dmotornyi <motorny2015@gmail.com>
2020-11-12 17:10:44 +0200

    Change

--- app.py
+++ app.py
@@ -1,37 +1,39 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/login')
 def login():
-    print(url_for('login') )
+#    print(url_for('login') )
     return render_template("login.html")
 
 @app.route('/index')
 def index():
-    print(url_for('index') )
+#    print(url_for('index') )
     return render_template("index.html")
 
 @app.route('/hardware')
 def about():
-    print(url_for('about') )
+#    print(url_for('about') )
     return render_template("hardware.html")
 
-@app.route('/new_input')
+@app.route('/new_input', methods=["POST","GET"] )
 def new_input():
-    print(url_for('new_input') )
-    return render_template("new_input.html")
+    if request.method == 'POST':
+        print(request.form)
+#        print(url_for('new_input') )
+        return render_template("new_input.html")
 
 
 @app.route('/db_change')
 def db_change():
-    print(url_for('db_change') )
+#    print(url_for('db_change') )
     return render_template("db_change.html")
 
 @app.route('/db_search')
 def db_search():
-    print(url_for('db_search') )
+#    print(url_for('db_search') )
     return render_template("db_search.html")
 
 
